4c6c12e3d4c630c39573e9b52f1b2fddc1f3a909
sanjana ahuja <sanjanaahuja0407@gmail.com>
2021-04-18 14:44:45 +0530

    initialcommit

---
+++ app/src/main/java/com/example/smartstick/Findthestick.java
@@ -0,0 +1,14 @@
+package com.example.smartstick;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Findthestick extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_findthestick);
+    }
+}
---
+++ app/src/main/java/com/example/smartstick/LocatethepersonActivity.java
@@ -0,0 +1,14 @@
+package com.example.smartstick;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LocatethepersonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.locatetheperson);
+    }
+}
