9fa01909cc04bd4b52eaa68c8171e5a29dcf5719
Dinesh <tarunchhabriya06>
2021-04-18 23:00:40 +0530

    Activated the register button

--- app/src/main/java/com/example/smartstick/LoginActivity.java
+++ app/src/main/java/com/example/smartstick/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.smartstick;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -41,6 +42,15 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+
+        register_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),RegistrationActivity.class));
+            }
+        });
+
+        
 
 
 
