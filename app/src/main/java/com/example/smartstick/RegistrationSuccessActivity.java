04fbe44973d5e278e11c68162e7970e5fef238da
Dinesh <tarunchhabriya06>
2021-04-19 09:35:14 +0530

    Go back to login button

--- app/src/main/java/com/example/smartstick/RegistrationSuccessActivity.java
+++ app/src/main/java/com/example/smartstick/RegistrationSuccessActivity.java
@@ -1,13 +1,29 @@
 package com.example.smartstick;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegistrationSuccessActivity extends AppCompatActivity {
+    Button go_back_button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registration_successful);
+
+        go_back_button = findViewById(R.id.go_back_button);
+
+        go_back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+            }
+        });
+
     }
 }
